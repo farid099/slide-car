47b755ead11792f2fcf8cce4df278f03acd06c5f
farid099 <34702069+farid099@users.noreply.github.com>
2018-10-25 18:21:48 +0400

    Add files via upload

---
+++ slide car/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 4,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        dots: false,
+        margin: 10,
+    });
+});
